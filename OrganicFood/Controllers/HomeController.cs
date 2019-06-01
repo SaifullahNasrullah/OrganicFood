ca3bfd8862e5cd5a23ba2cbf57b77a77871168eb
Saifullah <Saifullah@DESKTOP-TEM4MS9>
2019-06-01 16:29:10 +0300

    Organic Food Message

---
+++ OrganicFood/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OrganicFood.Models.Entity;
+
+namespace OrganicFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        TarimDatabaseEntities db = new TarimDatabaseEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLUrunler.ToList();
+
+            return View(degerler);
+        }
+
+        public ActionResult About()
+        { 
+
+            return View();
+        }
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {  
+            return View();
+        }
+    }
+}
